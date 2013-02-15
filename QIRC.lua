5d23028db2630e47ed3b724759d6f1d3155f84ca
Raezlyn <raezlyn.raymond@hotmail.com>
2013-02-15 15:07:03 -0600

    QIRC GET~!
    A basic irc program with an easy gui and simple to use! Working on
    /commands!

---
+++ QIRC.lua
@@ -0,0 +1,179 @@
+--[[ QIRC ]]--
+local version = "1.0.0"
+local author = "Raezlyn"
+local programName = "QIRC"
+
+function split(pString, pPattern)
+   local Table = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pPattern
+   local last_end = 1
+   local s, e, cap = pString:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+     table.insert(Table,cap)
+      end
+      last_end = e+1
+      s, e, cap = pString:find(fpat, last_end)
+   end
+   if last_end <= #pString then
+      cap = pString:sub(last_end)
+      table.insert(Table, cap)
+   end
+   return Table
+end
+
+Channel = "GLOBAL"
+Nickname = "Anon"
+ChatHistory = {}
+ChatScrolled = 0
+
+function cPrint ( nString )
+	ox,oy = term.getCursorPos()
+	scrWid, scrHei = term.getSize()
+	term.setCursorPos(scrWid/2-nString:len()/2, oy)
+	print(nString)
+end
+
+function MainDraw ()
+	term.clear()
+	term.setCursorPos(1,1)
+	term.setBackgroundColor(colors.black)
+	term.setTextColor(colors.white)
+	cPrint(programName)
+	term.setBackgroundColor(colors.blue)
+	scrWid, scrHei = term.getSize()
+	print(string.rep(" ",scrWid))
+	term.setBackgroundColor(colors.black)
+	scrWid, scrHei = term.getSize()
+	term.setCursorPos(2,scrHei)
+	write("> ")
+end
+
+function charRoll ()
+	scrWid, scrHei = term.getSize()
+	ChatBuffer = {}
+	if #ChatHistory > scrHei-3 then
+		for i=1, scrHei-3 do
+			ChatBuffer[#ChatBuffer+1] = ChatHistory[#ChatHistory-(scrHei-4)+i]
+		end
+	else
+		ChatBuffer = ChatHistory
+	end
+	for i=1, #ChatBuffer do
+		term.setCursorPos(2,2+i)
+		if ChatBuffer[i]:len() > scrWid-3 then
+			ChatBuffer[i] = ChatBuffer[i]:sub(1,scrWid-4)..".."
+		end
+		print(ChatBuffer[i]..string.rep(" ",(scrWid-2)-ChatBuffer[i]:len()))
+	end
+end
+
+commands = {}
+
+function parseCommand ( nString )
+	nAR = split(nString, " ")
+	nAR[1] = nAR[1]:sub(2,nAR[1]:len())
+	if commands[nAR[1]] ~= nil then
+		commands[nAR[1]](unpack(nAR,2))
+	else
+		ChatHistory[#ChatHistory+1] = "No such command!"
+	end
+end
+
+function chkT ( nString )
+	if nString:sub(1,1) == "/" then return true else return false end
+end
+
+function broadcastN ( nString )
+	rednet.broadcast(Nickname..": "..nString)
+end
+
+function recN ()
+	while true do
+		act, sed, txt = os.pullEvent()
+		if act == "rednet_message" then
+			ChatHistory[#ChatHistory+1] = "("..tostring(sed)..") "..txt
+			charRoll()
+		end
+	end
+end
+
+function setUp ()
+	for n,m in ipairs(rs.getSides())
+		rednet.close(m)
+		rednet.open(m, Channel)
+	end
+end
+
+function cread ()
+	while true do
+		if nStringd == nil then
+			nStringd = ""
+		end
+		scrWid, scrHei = term.getSize()
+		if nString:len() > noption:len()+4 then
+			nBufferString = nStringd:sub(nString:len()-(scrWid-3),nStringd:len())
+		else
+			nBufferString = nStringd..string.rep(" ",(scrWid-3)-nStringd:len())
+		end
+		term.setCursorPos(3,scrHei)
+		write(nBufferString)
+		action, char = os.pullEvent()
+		if action == "char" then
+			nString = nStringd..char
+		end
+		if action == "key" then
+			if char == keys.backspace then
+				if nString:len() > 0 then
+					nStringd = nStringd:sub(1,nString:len()-1)
+				end
+			end
+			if char == keys.space then
+				nStringd = nStringd.." "
+			end
+			if char == keys.delete then
+				nStringd = ""
+			end
+			if char == keys.enter then
+				break
+			end
+		end
+	end
+	return nStringd
+end
+
+function cN ()
+	while true do
+		nmsg = cread()
+		if nmsg == "/exit" then break end
+		if nmsg == "" or nmsg == " " then
+			ChatHistory[#ChatHistory+1]="Can't send empty message!"
+			chatRoll()
+		else
+			if chkT(nmsg) == false then
+				broadcastN(nmsg)
+			else
+				pasreCommand(nmsg)
+			end
+		end
+	end
+end
+
+MainDraw()
+
+parallel.waitForAny(
+	cN,
+	recN
+	)
+
+term.clear()
+term.setCursorPos(1,1)
+scrWid, scrHei = term.getSize()
+term.setBackgroundColor(colors.black)
+term.setTextColor(colors.white)
+for i=1, scrHei do
+print(string.rep(" ",scrWid))
+end
+term.setCursorPos(1,1)
+cPrint("Thanks for using this!")
+cPrint("~Raezlyn")
